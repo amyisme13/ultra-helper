52098d35c6bf504390f9b1864c4a6d70fbecbd77
Azmi Makarima <amy.azmim@gmail.com>
2020-08-16 21:39:04 +0700

    add scopes to UltraUser

--- src/Models/UltraUser.php
+++ src/Models/UltraUser.php
@@ -36,4 +36,40 @@ class UltraUser extends Model
         'activated_at' => 'datetime',
         'suspended' => 'boolean',
     ];
+
+    /**
+     * Scope a query to only containing the given keyword
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $keyword
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSearch($query, $keyword)
+    {
+        if (empty($keyword)) {
+            return $query;
+        }
+
+        return $query->where(function ($query) use ($keyword) {
+            $query
+                ->where('name', 'like', "%$keyword%")
+                ->orWhere('username', 'like', "%$keyword%");
+        });
+    }
+
+    /**
+     * Scope a query to only include the given division
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param string $division
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDivision($query, $division)
+    {
+        if (empty($division)) {
+            return $query;
+        }
+
+        return $query->where('division', $division);
+    }
 }
